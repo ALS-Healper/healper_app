8cb644462680c9f214ceecf806196f75416b0c39
ALS-Healper <als@healper.dk>
2022-11-16 12:30:27 +0100

    Added fetch post for question answer

--- app/pages/Questionnaire.svelte
+++ app/pages/Questionnaire.svelte
@@ -2,21 +2,41 @@
     import { goBack } from 'svelte-native'
     import { onMount } from 'svelte'
 
-    let currentQuestion;
+    let currentQuestion = {question_text: "Loading questions"};
     let questionIndex = 0;
-    let questionList = []
+    let questionList = [];
+
+    let answer;
 
     onMount(async ()=>{
        const res = await fetch("http://10.0.2.2:8080/questionnaires/");
        const data = await res.json()
        questionList = data.results[0].questions
-       currentQuestion = data.results[0].questions[0].question_text
-    })
+       currentQuestion = data.results[0].questions[0]
+    });
 
     function onAnswerTap(){
+        createQuestionEntry("http://10.0.2.2:8080/questionEntries/",{response_text: answer, 
+            creator:1, 
+            question: 1              
+        })
+        
         questionIndex += 1
-        currentQuestion = questionIndex >= questionList.length ? "Tak for dit svar" : questionList[questionIndex].question_text
-    }
+        currentQuestion = questionIndex >= questionList.length ? "Tak for dit svar" : questionList[questionIndex]
+    };
+
+    async function createQuestionEntry(url, data) {
+        alert(data.response_text)
+        const response = await fetch(url, {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data) 
+        });
+     return response.json(); 
+    };
 
 </script>
 
@@ -24,15 +44,9 @@
     <actionbar title="Client questionnaire page" />
     <stackLayout>
         <label class="selectionText">Questions from your psychologist</label>
-        <label text="{currentQuestion}" class="questionText" textWrap="true" />
+        <label text="{currentQuestion.question_text}" class="questionText" textWrap="true" />
         <flexBoxLayout flexWrap="wrap" justifyContent="center">
-            <button text="Answer1" />
-            <button text="Answer2" />
-            <button text="Answer3" />
-            <button text="Answer4" />
-            <button text="Answer5" />
-            <button text="Answer6" />
-            <button text="Answer7" />
+           <textfield bind:text="{answer}" hint="Enter your answer here"/>
         </flexBoxLayout>
         <!--Skal ind på diary page-->
         <button text="Nothing in particular" class="button" on:tap={onAnswerTap}/>
