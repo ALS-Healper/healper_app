c64b1d9cfde5e383cc8c72e1462749072fa7b69f
marku <markusharen@gmail.com>
2022-11-17 14:55:07 +0100

    Navbar to homepage, and questions options added.

--- app/pages/Home.svelte
+++ app/pages/Home.svelte
@@ -1,20 +1,63 @@
 <script>
     import RoleSelect from './RoleSelect.svelte'
 	import { navigate } from 'svelte-native'
-    import Navbar from './components/navbar.svelte';
+    import Questionnaire from './Questionnaire.svelte'
    
 </script>
 
 <page>
-    <stackLayout>
+    <!-- <stackLayout>
         <image src="https://cdn-icons-png.flaticon.com/512/149/149071.png" class="profileImage" />
         <button text="Go to Diary" on:tap="{ () => navigate({ page: RoleSelect }) }" />
-    </stackLayout>
+    </stackLayout> -->
+    <gridLayout rows="200, 100, *, 80" columns="*">
+
+        <image src="https://cdn-icons-png.flaticon.com/512/149/149071.png" class="profileImage" row="0" col="0" />
+        <button text="Go to Diary" on:tap="{ () => navigate({ page: RoleSelect }) }"  row="1" col="0"/>
+        <label text="Feed" class="info" row="2" col="0"/>
+        <flexboxLayout orientation="horizontal" row="3" col="0" class="navBar" justifyContent="space-around">
+            <button class="navButton">
+                <formattedString>
+                    <span class="fas" text="&#xf135;\n"/>
+                    <span text="Home"/>
+                </formattedString>
+            </button>
+            <button class="navButton">
+                <formattedString>
+                    <span class="fas" text="&#xf135;\n"/>
+                    <span text="Menu"/>
+                </formattedString>
+            </button>
+            <button class="navButton" on:tap="{ () => navigate({ page: Questionnaire }) }">
+                <formattedString>
+                    <span class="fas" text="&#xf135;\n"/>
+                    <span text="Settings"/>
+                </formattedString>
+            </button>
+        </flexboxLayout>
+    </gridLayout>
 </page>
 
 <style>
-    .info .fas {
+    .fas {
         color: #3A53FF;
+    }
+
+    .navBar{
+        background-color: aqua;
+        align-items: center;
+        border: 5px solid black;
+        border-radius: 30px;
+        
+    }
+
+    .navButton{
+        margin: 0;
+        border-color: transparent;
+        background-color: transparent;
+        border-width: 1;
+        z-index: 0;
+        font-size: 15;
     }
 
     .info {
@@ -25,8 +68,8 @@
 
     .profileImage {
         border-radius: 50%;
-        width: 150px;
-        height: 150px;
+        width: 200px;
+        height: 200px;
         margin: 100px;
     }
 
--- app/pages/Questionnaire.svelte
+++ app/pages/Questionnaire.svelte
@@ -8,26 +8,29 @@
     let questionList = [];
 
     let answer;
+    let sliderValue = 0;
 
     onMount(async ()=>{
        const res = await fetch("http://10.0.2.2:8080/questionnaires/");
        const data = await res.json()
-       questionList = data.results[0].questions
-       currentQuestion = data.results[0].questions[0]
+       questionList = [...data.results[0].inputquestions, ...data.results[0].choicequestions, ...data.results[0].numericquestions]
+    // Hvis det her virker skal der måske laves en sortering som sætter dem i den rigtige rækkefølge for questionnairen
+       currentQuestion = data.results[0].inputquestions[0]
     });
 
     function onAnswerTap(){
         createQuestionEntry("http://10.0.2.2:8080/questionEntries/",{response_text: answer, 
             creator:1, 
-            question: 1              
+            question: questionIndex              
         })
         
         questionIndex += 1
-        currentQuestion = questionIndex >= questionList.length ? "Tak for dit svar" : questionList[questionIndex]
+        currentQuestion = questionIndex >= questionList.length ? {question_text: "Tak for dit svar"} : questionList[questionIndex]
+        //alert(questionIndex +" "+ currentQuestion.question_text)
     };
 
     async function createQuestionEntry(url, data) {
-        alert(data.response_text)
+        //alert(data.response_text)
         const response = await fetch(url, {
             method: 'POST',
             mode: 'cors',
@@ -39,6 +42,11 @@
      return response.json(); 
     };
 
+    function sliderValueChange(args){
+        const slider = args.object
+        sliderValue = slider.value
+    }
+
 </script>
 
 <page>
@@ -47,15 +55,34 @@
         <label class="selectionText">Questions from your psychologist</label>
         <label text="{currentQuestion.question_text}" class="questionText" textWrap="true" />
         <flexBoxLayout flexWrap="wrap" justifyContent="center">
-           <textfield bind:text="{answer}" hint="Enter your answer here"/>
-           {#if currentQuestion.options}
-            {#each currentQuestion.options as option}
-                <button text="{option.option_text}" class="button"/>
+           {#if currentQuestion.optioninputs}
+            {#each currentQuestion.optioninputs as option}
+            <textfield bind:text="{answer}" hint="{option.standard_text}"/>
+            {/each}
+            <button text="Submit answer" class="button" on:tap={onAnswerTap}/>
+           {/if}
+           {#if currentQuestion.optionchoices}
+            {#each currentQuestion.optionchoices as option}
+                <button bind:text="{answer = option.option_value}" class="button" on:tap={onAnswerTap}/>
             {/each}
            {/if}
+           {#if currentQuestion.optionnumerics}
+           {#each currentQuestion.optionnumerics as option}
+           <label text="{sliderValue}" class="sliderValue"/>
+           <stackLayout orientation="horizontal" class="sliderField">
+                <label text="{option.min_value}"/>
+                <slider minValue="{option.min_value}" 
+                maxValue="{option.max_value}"
+                value="{option.max_value/2}"
+                on:valueChange="{sliderValueChange}" width=80%/>
+                <label text="{option.max_value}"/>
+           </stackLayout>
+            <button text="Submit answer" class="button" on:tap={onAnswerTap}/>
+            {/each}
+            {:else}
+                <button text="Back" class="button" on:tap="{goBack}"/>
+           {/if}
         </flexBoxLayout>
-        <!--Skal ind på diary page-->
-        <button text="Submit answer" class="button" on:tap={onAnswerTap}/>
     </stackLayout>
 </page>
 
@@ -78,5 +105,14 @@
         margin: 30;
         font-size: 20;
         font-style: italic;
+    }
+    .sliderValue{
+        font-size: 20;
+        horizontal-align: center;
+        vertical-align: center;
+    }
+    .sliderField{
+        width: 80%;
+        font-size: 10;
     }
 </style>
