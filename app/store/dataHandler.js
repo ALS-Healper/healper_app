e1955912a346cf7600ea8847f0f6a42e97ca5f12
mettemariehws <mettemhws@gmail.com>
2022-11-22 13:17:11 +0100

    Secure store created

---
+++ app/store/dataHandler.js
@@ -0,0 +1,35 @@
+async function getData(url, token){
+    const response =  await fetch(url, {
+        headers: {
+            "Content-type": "application/json",
+            "Authorization": `Token ${token}`
+        },
+        method: "GET"
+    });
+    return await response.json(); 
+};
+
+async function postData(url, data){
+    const response = await fetch(url, {
+        header: {
+            "content-type": "applikation/json"
+        },
+        method: "POST",
+        mode: "cors",
+        body: {data}
+    });
+
+    return response.json();
+};
+
+function formatDates(stringDate){
+
+    let date = new Date(stringDate);
+
+    let formatDate = date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear(); 
+
+    return formatDate;
+
+}
+
+export default {getData, postData, formatDates};
